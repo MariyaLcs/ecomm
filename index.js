25cda998ec902afe6e39d1bbb2f3be6bd47694d3
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-10 12:11:46 +0000

    Signing Out a user

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(`
   <div>
   HELLO ${req.session.userId}!
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -41,6 +41,27 @@ app.post("/", async (req, res) => {
   req.session.userId = user.id;
   res.send("Account Created!!!");
 });
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out!");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(
+    `
+  <div> 
+    <form method="post">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />        
+        <button>Sign In</button>
+    </form>
+  </div>
+  `
+  );
+});
+
+app.post("/signin", async (req, res) => {});
 
 app.listen(3000, () => {
   console.log("Listening");

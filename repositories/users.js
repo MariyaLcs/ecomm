8f877724d3f7b50bf99a4f6cb5c617c808d012ee
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-10 08:19:09 +0000

    Opening the Repo Data file

--- repositories/users.js
+++ repositories/users.js
@@ -12,6 +12,20 @@ class UsersRepository {
       fs.writeFileSync(this.filename, "[]");
     }
   }
-}
+  async getAll() {
+    //Open the file called this.filename
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: "utf8"
+    });
+    //Read its contents
+    console.log(contents);
+    //Parse the contents
 
-const repo = new UsersRepository("users.json");
+    //Return the parsed data
+  }
+}
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+  await repo.getAll();
+};
+test();

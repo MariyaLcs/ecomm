ad2f540bef442db9f84ab887f08b55921a77183a
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-13 10:46:37 +0000

    Redirect on Success Actions

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -27,7 +27,7 @@ router.post(
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
-    res.send("Account Created!!!");
+    res.redirect("/admin/products");
   }
 );
 
@@ -49,7 +49,7 @@ router.post(
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
-    res.send("You are signed in!!!");
+    res.redirect("/admin/products");
   }
 );
 

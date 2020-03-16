84007b6ab2cc0dddac5b80c92859bc86771d7ec9
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-16 15:42:56 +0000

    Redirect on remove

--- routes/carts.js
+++ routes/carts.js
@@ -32,7 +32,7 @@ router.post("/cart/products", async (req, res) => {
     items: cart.items
   });
 
-  res.send("Product added to cart");
+  res.redirect("/cart");
 });
 //Receive a get request to show all items in cart
 router.get("/cart", async (req, res) => {
@@ -48,7 +48,13 @@ router.get("/cart", async (req, res) => {
 });
 //Receive a post request to delete an item from a cart
 router.post("/cart/products/delete", async (req, res) => {
-  console.log(req.body.productId);
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter(item => item.id !== itemId);
+  await cartsRepo.update(req.session.cartId, { items });
+
+  res.redirect("/cart");
 });
 
 module.exports = router;

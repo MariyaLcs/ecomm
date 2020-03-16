47b326a6cdc929c164b4193a921f6bd131631fe1
Mariya Lucas <mariya.podosinova@gmail.com>
2020-03-16 14:12:31 +0000

    Add to cart functionality added

--- repositories/carts.js
+++ repositories/carts.js
@@ -2,10 +2,4 @@ const Repository = require("./repository");
 
 class CartsRepository extends Repository {}
 
-//Receive a post request to add an item to a cart
-
-//Receive a get request to show all items in cart
-
-//Receive a post request to delete an item from a cart
-
 module.exports = new CartsRepository("carts.json");
